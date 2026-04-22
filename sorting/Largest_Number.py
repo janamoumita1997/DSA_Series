84d7f9bf1849c289a683304e6894f922098c2f58
janamoumita1997 <jana12moumita@gmail.com>
2026-04-22 12:58:03 +0530

    Largest Number

--- sorting/Largest_Number.py
+++ sorting/Largest_Number.py
@@ -16,8 +16,28 @@ Input: nums = [3,30,34,5,9]
 Output: "9534330"
 
 """
-# nums = [3,30,34,5,9]
-# nums.sort()
-# max_num = ""
+from typing import List
+from functools import cmp_to_key
 
+nums = [3,30,34,5,9]
+def find_largest(nums):
+    str_nums = list(map(str, nums))
 
+    # Define a comparator that decides order based on the concatenation result
+    def compare(x: str, y: str) -> int:
+        if x + y > y + x:
+            return -1
+        elif x + y < y + x:
+            return 1
+        else:
+            return 0
+
+    # Sort the numbers using the custom comparator
+    str_nums.sort(key=cmp_to_key(compare))
+
+    # Join the sorted numbers to form the largest number
+    result = ''.join(str_nums)
+
+    # If the largest number is '0' (i.e., multiple zeroes), return '0'
+    return '0' if result[0] == '0' else result
+print(find_largest(nums))
