b5ec8191aa3f6ba385176416adae8cf106bba6fe
janamoumita1997 <jana12moumita@gmail.com>
2026-05-03 23:32:35 +0530

    sliding windows

---
+++ sliding_window/findMaxAverage.py
@@ -0,0 +1,37 @@
+"""
+You are given an integer array nums consisting of n elements, and an integer k.
+
+Find a contiguous subarray whose length is equal to k that has the maximum average value and return this value. Any answer with a calculation error less than 10-5 will be accepted.
+
+ 
+
+Example 1:
+
+Input: nums = [1,12,-5,-6,50,3], k = 4
+Output: 12.75000
+Explanation: Maximum average is (12 - 5 - 6 + 50) / 4 = 51 / 4 = 12.75
+Example 2:
+
+Input: nums = [5], k = 1
+Output: 5.00000
+"""
+from typing import List
+
+nums = []
+k = 0
+
+def findMaxAverage(self, nums: List[int], k: int) -> float:
+    n = len(nums)
+    if k == 0 or n == 0:
+        return 'not allowed'
+        
+    current_sum = sum(nums[:k])
+    max_sum = current_sum
+    if k == n:
+        return round(max_sum/k,5)
+
+    for r in range(k,n):
+        current_sum += nums[r] - nums[r-k]
+        max_sum = max(max_sum, current_sum)
+
+    return round(max_sum / k,5)
