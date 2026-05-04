e5820e373d178588ef23c195dc61424e4927c001
janamoumita1997 <jana12moumita@gmail.com>
2026-05-04 23:48:26 +0530

    prefix sum question--

--- prefix_sum/subarraySum.py
+++ prefix_sum/subarraySum.py
@@ -14,3 +14,19 @@ Example 2:
 Input: nums = [1,2,3], k = 3
 Output: 2
 """
+nums = [1,-1,0]
+k = 0
+
+count = 0
+n = len(nums)
+for i in range(1,n):
+    nums[i] += nums[i-1]
+
+nums = [0] + nums
+seen_list  = {0:0}
+for i,val in enumerate(nums):
+    if (val - k) in seen_list:
+        count += 1
+    seen_list[val] = i
+print(count)
+
