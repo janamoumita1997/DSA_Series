b896a08d0b770ec3b90a2d9d09462765510d7e53
janamoumita1997 <jana12moumita@gmail.com>
2026-05-17 22:44:20 +0530

    isPerfectSquare

---
+++ Binary_search/perfect_square.py
@@ -0,0 +1,16 @@
+from typing import List
+def isPerfectSquare(num: int) -> bool:
+    if num < 2:
+        return True
+    left,right = 1, num//2
+    while left<=right:
+        mid = (left+right)//2
+        square = mid * mid
+        if square == num:
+            return True
+        elif square < num:
+            left = mid+1
+        else:
+            right = mid-1
+    return False
+print(isPerfectSquare(13))
