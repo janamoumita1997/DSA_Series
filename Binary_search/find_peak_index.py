8992e0d4f82075111c85b2204e978ff4c1849a2c
janamoumita1997 <jana12moumita@gmail.com>
2026-06-01 23:00:51 +0530

    --

--- Binary_search/find_peak_index.py
+++ Binary_search/find_peak_index.py
@@ -1,12 +1,17 @@
 arr = [0,1,0]
 
-left = 0
-right = len(arr) - 1
+def find_peak(arr):
+    n = len(arr)
+    if n <= 2:
+        return -1
+    
+    left = 0
+    right = n - 1
 
-while left < right:
-    mid = (left + right)//2
-    if arr[mid] < arr[mid+1]:
-        left = mid + 1
-    else:
-        right = mid
-print(left)
+    while left < right:
+        mid = (left + right)//2
+        if arr[mid] < arr[mid+1]:
+            left = mid + 1
+        else:
+            right = mid
+    return left
