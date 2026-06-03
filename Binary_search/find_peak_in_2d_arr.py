3f13b1cb06b68ab2debae4420cd2e7a9a6a4b91c
janamoumita1997 <jana12moumita@gmail.com>
2026-06-03 23:37:51 +0530

    --

--- Binary_search/find_peak_in_2d_arr.py
+++ Binary_search/find_peak_in_2d_arr.py
@@ -1,3 +1,30 @@
-mat = [[10, 20, 15],
-        [21, 30, 14],
-        [7,  16, 32]]
+mat = [[10, 3, 15],
+        [21, 3, 34],
+        [70,  16, 32]]
+
+def find_peak_in_2D(mat):
+    n = len(mat)
+    m = len(mat[0])
+
+    low = 0
+    high = m-1
+    mini = -float("inf")
+    while low <= high:
+        mid = (low + high)//2
+        max_row = 0
+        for i in range(1,n):
+            if mat[i][mid] > mat[max_row][mid]:
+                max_row = i
+        
+        left = mat[max_row][mid-1] if mid>0 else mini
+        right = mat[max_row][mid+1] if mid+1<m else mini
+        
+        if mat[max_row][mid]>= left and mat[max_row][mid]>= right:
+            return [max_row,mid]
+        elif right >  mat[max_row][mid]:
+            left = mid + 1
+        else:
+            right = mid - 1
+    return [-1,-1]
+
+print(find_peak_in_2D(mat))
