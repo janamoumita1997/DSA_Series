ac0d81f2a048a731585cf4de8f1627bdd68fab75
janamoumita1997 <jana12moumita@gmail.com>
2026-06-10 00:00:04 +0530

    ---

--- String/ransom_note.py
+++ String/ransom_note.py
@@ -2,20 +2,11 @@ ransomNote = "aa"
 magazine = "aab"
 
 def main(magazine, ransomNote):
-    magazine_words = {}
-    for i in magazine:
-        magazine_words[i] = magazine_words.get(i,0) + 1
-
-    for j in ransomNote:
-        if j not in magazine_words:
+    for i in ransomNote:
+        if i not in magazine:
             return False
-        else:
-            magazine_words[j] -= 1
-            
-        if magazine_words[j] == 0:
-            del magazine_words[j]
+        magazine = magazine.replace(i,"",1)
     return True
-
 
 
 print(main(magazine, ransomNote))
