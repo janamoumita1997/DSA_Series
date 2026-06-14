bf38ac389d7134307a291489eea3c074373ce446
janamoumita1997 <jana12moumita@gmail.com>
2026-06-14 23:15:26 +0530

    find anagrame updated corrected version

--- String/find_anagrame.py
+++ String/find_anagrame.py
@@ -1,34 +1,29 @@
-s = "abab"
-p = ""
+s = "ababaa"
+p = "ab"
+ns, np = len(s), len(p)
 
-def is_anagrame(p,s):
-    if len(p) != len(s):
-        return False
-    
-    count = {}
-    for i in p:
-        count[i] = count.get(i,0) + 1
 
-    for j in s:
-        count[j] = count.get(j,0) - 1  
+# Frequency arrays for 26 letters
+freqP = [0] * 26
+freqS = [0] * 26
 
-    return all(i == 0 for i in count.values())
+# Build frequency for pattern p
+for char in p:
+    freqP[ord(char) - ord('a')] += 1
 
-   
-def find_anaIndex(s,p):
+# First window in s
+for i in range(np):
+    freqS[ord(s[i]) - ord('a')] += 1
 
-    ans = []
-    window_size = len(p)
-    if window_size == 0:
-        return ans
-    window = s[:window_size]
-    if is_anagrame(p,window):
-        ans.append(0)
-    for i in range(window_size,len(s)):
-        window = window[1:] + s[i]
-        if is_anagrame(p,window):
-            ans.append(i-window_size+1)
-    return ans
+result = []
+if freqS == freqP:
+    result.append(0)
 
-print(find_anaIndex(s,p))
+# Slide the window
+for i in range(np, ns):
+    freqS[ord(s[i]) - ord('a')] += 1
+    freqS[ord(s[i - np]) - ord('a')] -= 1
+    if freqS == freqP:
+        result.append(i - np + 1)
+
 
