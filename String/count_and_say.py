5b72c147276363f3916d7f67aa4da4edbd63df16
janamoumita1997 <jana12moumita@gmail.com>
2026-06-22 00:04:54 +0530

    correct version

--- String/count_and_say.py
+++ String/count_and_say.py
@@ -1,29 +1,28 @@
 n = 4
-# def RLE(n:str) -> str:
-#     left = 0
-#     seen = n[0]
-#     encoded_str = ''
-#     for right, val in enumerate(n):
-#         if val != seen:
-#             encoded_str += f"{right-left}{seen}"
-#             seen = val
-#             left = right
-#     return encoded_str
-# print(RLE("3322251"))
-
-from collections import Counter
-
-def RLE(n:str)->str:
+def RLE(n:str) -> str:
+    count = 1
     encoded_str = ''
-    for k,v in dict(Counter(n)).items():
-        encoded_str += f"{v}{k}"
+   
+    for i in range(1,len(n)):
+        if n[i] == n[i-1]:
+            count += 1
+
+        else:
+            encoded_str += str(count)+n[i-1]
+            count = 1
+            
+    encoded_str += str(count) + n[-1]
     return encoded_str
-# print(RLE("1"))
+
+
+
+
+
 
 def count_and_say(n:int)->str:
     if n == 1:
         return "1"
     res = RLE(count_and_say(n-1))
     return res
-print(count_and_say(1))
+print(count_and_say(5))
 
