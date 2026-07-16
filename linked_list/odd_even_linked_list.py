f6d499d82b7802c7aa84f832f676176c0123e18a
janamoumita1997 <jana12moumita@gmail.com>
2026-07-16 23:56:08 +0530

    --

--- linked_list/odd_even_linked_list.py
+++ linked_list/odd_even_linked_list.py
@@ -7,3 +7,25 @@ Note that the relative order inside both the even and odd groups should remain a
 
 You must solve the problem in O(1) extra space complexity and O(n) time complexity.
 """
+from typing import Optional
+head = [2,1,3,5,6,4,7]
+class ListNode:
+    def __init__(self, val: int = 0, next: Optional['ListNode'] = None):
+        self.val = val
+        self.next = next
+def oddEvenList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+    if not head or not head.next:
+        return head
+
+    odd = head
+    even = head.next
+    evenHead = even
+
+    while even and even.next:
+        odd.next = even.next
+        odd = odd.next
+        even.next = odd.next
+        even = even.next
+
+    odd.next = evenHead
+    return head
