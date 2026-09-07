e08a3f0708a783448a21136dc23d2d53b05425c5
janamoumita1997 <jana12moumita@gmail.com>
2026-09-08 00:21:16 +0530

    using multiple stack

--- stack_and_queue/design_problem/maximum_frequency_track.py
+++ stack_and_queue/design_problem/maximum_frequency_track.py
@@ -40,50 +40,70 @@
 #         return x
 
 
+# class FreqStack:
+#     def __init__(self):
+#         self.st1 = []
+        
+#         self.freqCount = {}
+
+#     def getMaxCandidates(self, freqCout):
+#         maxVal = max(self.freqCount.values())
+#         maxCandidates = [i for i in self.freqCount if self.freqCount[i] == maxVal]
+#         return maxCandidates
+    
+#     def push(self, val) -> None:
+#         self.st1.append(val)
+#         self.freqCount[val] = self.freqCount.get(val,0) + 1
+
+#     def pop(self) -> int:
+#         st2 = []
+#         if len(self.st1) == 0:
+#             return -1
+#         possible_candidates = self.getMaxCandidates(self.freqCount)
+#         if self.st1[-1] in possible_candidates:
+#             x = self.st1[-1]
+#             self.st1.pop()   
+#         else:
+#             while len(self.st1)>0:
+#                 temp = self.st1.pop()
+#                 if temp in possible_candidates:
+#                     x = temp
+#                     break
+#                 st2.append(temp)
+#             self.st1 = self.st1 + st2[::-1]
+#         self.freqCount[x] = self.freqCount.get(x,0) - 1
+#         return x
+                
+# freq = FreqStack()
+# freq.push(5)
+# freq.push(1)
+# freq.push(2)
+# freq.push(5)
+# freq.push(5)
+# freq.push(5)
+# freq.push(1)
+# freq.push(6)
+# freq.push(1)
+# freq.push(5)
+# for i in range(10):
+#     print(freq.pop())
+
 class FreqStack:
     def __init__(self):
-        self.st1 = []
-        
-        self.freqCount = {}
+        self.freq = {}          # val -> count
+        self.group = {}         # count -> stack of vals with that count
+        self.maxFreq = 0
 
-    def getMaxCandidates(self, freqCout):
-        maxVal = max(self.freqCount.values())
-        maxCandidates = [i for i in self.freqCount if self.freqCount[i] == maxVal]
-        return maxCandidates
-    
-    def push(self, val) -> None:
-        self.st1.append(val)
-        self.freqCount[val] = self.freqCount.get(val,0) + 1
+    def push(self, val: int) -> None:
+        f = self.freq.get(val, 0) + 1
+        self.freq[val] = f
+        if f > self.maxFreq:
+            self.maxFreq = f
+        self.group.setdefault(f, []).append(val)
 
     def pop(self) -> int:
-        st2 = []
-        if len(self.st1) == 0:
-            return -1
-        possible_candidates = self.getMaxCandidates(self.freqCount)
-        if self.st1[-1] in possible_candidates:
-            x = self.st1[-1]
-            self.st1.pop()   
-        else:
-            while len(self.st1)>0:
-                temp = self.st1.pop()
-                if temp in possible_candidates:
-                    x = temp
-                    break
-                st2.append(temp)
-            self.st1 = self.st1 + st2[::-1]
-        self.freqCount[x] = self.freqCount.get(x,0) - 1
-        return x
-                
-freq = FreqStack()
-freq.push(5)
-freq.push(1)
-freq.push(2)
-freq.push(5)
-freq.push(5)
-freq.push(5)
-freq.push(1)
-freq.push(6)
-freq.push(1)
-freq.push(5)
-for i in range(10):
-    print(freq.pop())
+        val = self.group[self.maxFreq].pop()
+        self.freq[val] -= 1
+        if not self.group[self.maxFreq]:
+            self.maxFreq -= 1
+        return val
